1648c72f272e9e199eb2c997c452cd4c21e5cb8f
Vaggelis Boutas <vaggbout@hotmail.com>
2023-03-19 15:09:20 +0200

    use plural

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
 import * as express from "express";
 import * as RegisterApiController from "./controllers/backoffice/api/register";
 import * as LoginApiController from "./controllers/backoffice/api/login";
-import * as StoreApiController from "./controllers/backoffice/api/store";
+import * as StoreApiController from "./controllers/backoffice/api/stores";
 import * as Validators from "./middlewares/validators";
 import * as UserMiddleware from "./middlewares/user";
 
